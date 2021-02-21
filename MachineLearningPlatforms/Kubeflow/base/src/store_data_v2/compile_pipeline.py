3f42903b94e088320ac2967fdb94c78778bcfd03
IvanNardini <ivan.nardini92@gmail.com>
2021-02-21 15:56:21 +0100

    test component annotation and prebuild image

---
+++ MachineLearningPlatforms/Kubeflow/base/src/store_data_v2/compile_pipeline.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+# pipeline.py
+# pipeline is an example of how creating components from existing application code
+# Improvements:
+# - Wrapper python and annotations to simplify the pipeline
+# - file_outputs option to avoid .add_pvolume repetitions
+# Steps:
+# Assuming that the Docker container image of the existing app is uploaded to a registry:
+# 1 - Write a component function using the Kubeflow Pipelines DSL
+# 2 - Write a pipeline function using the Kubeflow Pipelines DSL
+# 3 - Compile the pipeline to generate a compressed YAML definition of the pipeline
+
+import kfp
+import kfp.components as cpt
+import kfp.compiler as cmp
+import kfp.dsl as dsl
+import argparse
+
+# Variables ------------------------------------------------------------------------------------------------------------
+REGISTRY = "docker.io/in92"
+
+
+# Components -----------------------------------------------------------------------------------------------------------
+
+@kfp.dsl.component
+def prepare_component(text_path: dsl.PipelineParam, out_path_pkl: dsl.PipelineParam):
+    return kfp.dsl.ContainerOp(
+        name='Prepare data component',
+        image=f'{REGISTRY}/kf_prepare:1.0.0',
+        arguments=['--text-path', text_path,
+                   '--path-pkl', out_path_pkl],
+        file_outputs={
+            'output': '/data-processed'}
+    )
+
+
+@kfp.dsl.component
+def count_component(input_path_pkl: dsl.PipelineParam, word: str):
+    return kfp.dsl.ContainerOp(
+        name='Count word component',
+        image=f'{REGISTRY}/kf_count_word:1.0.0',
+        arguments=['--path-pkl', input_path_pkl,
+                   '--word', word]
+    )
+
+
+# Main
+def main(args):
+    output_pipeline_dir = args.output_pipeline_dir
+
+    @dsl.pipeline(name="Store data pipeline",
+                  description="A pipeline to test volume mounting")
+    def build_pipeline(text_bucket_path: dsl.PipelineParam, pkl_bucket_path: dsl.PipelineParam, word="Kubeflow"):
+        step_1 = prepare_component(text_path=text_bucket_path, out_path_pkl=pkl_bucket_path)
+        step_2 = count_component(input_path_pkl=step_1.output, word=word)
+        step_2.after(step_1)
+
+    pipeline_compiler = cmp.Compiler()
+    pipeline_compiler.compile(pipeline_func=build_pipeline,
+                              package_path=f'{output_pipeline_dir}/store_data_pipeline_v2.zip')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Run pipeline builder")
+    parser.add_argument('--output-pipeline-dir', default='../../out/pipelines')
+    args = parser.parse_args()
+    main(args=args)
---
+++ MachineLearningPlatforms/Kubeflow/base/src/store_data_v2/count.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# count.py
+# count is the count module of a better version of store_data
+# Improvements:
+# - Wrapper python and annotations to simplify the pipeline
+# - file_outputs option to avoid .add_pvolume repetitions
+
+import pickle
+import argparse
+
+
+# Helpers --------------------------------------------------------------------------------------------------------------
+
+def load_data(input_path_pkl: str) -> list:
+    with open(input_path_pkl, 'rb') as wl:
+        word_list = pickle.load(wl)
+    return word_list
+
+
+# Functions ------------------------------------------------------------------------------------------------------------
+
+def count_word(word_list: list, word: str) -> int:
+    return word_list.count(word)
+
+
+# Main -------------------------------------------------------------------------------------------------------------
+def main(args):
+    input_path_pkl = args.path_pkl
+    word = args.word
+
+    token_text = load_data(input_path_pkl=input_path_pkl)
+    n_word = count_word(word_list=token_text, word=word)
+    return n_word
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run pipeline builder")
+    parser.add_argument('--path-pkl', help='The path to load pkl file with processed data')
+    parser.add_argument('--word', help='The word to count occurences')
+    args = parser.parse_args()
+    main(args=args)

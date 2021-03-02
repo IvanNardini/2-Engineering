99402c1dea2f81da057e8113f82a74e094d35547
IvanNardini <ivan.nardini92@gmail.com>
2021-03-02 21:50:57 +0100

    component locally tested!

--- MachineLearningPlatforms/Kubeflow/base/pipelines/store_data_v2/components/prepare/src/component.py
+++ MachineLearningPlatforms/Kubeflow/base/pipelines/store_data_v2/components/prepare/src/component.py
@@ -7,39 +7,61 @@
 # For portability, I prefer tf.gfile because you can debug your model
 # locally and train it on cloud without changing a single line.
 
-from tensorflow import gfile
+from tensorflow.io import gfile
 from prepare import run_prepare
 import pickle
 import logging
+import argparse
 
 # Settings
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
                     datefmt='%m/%d/%Y %I:%M:%S %p',
                     level=logging.DEBUG)
 
+
 # Helpers --------------------------------------------------------------------------------------------------------------
 def load_text(file_path: str) -> str:
-    with gfile.Open(file=file_path, mode='r') as file:
+    with gfile.GFile(name=file_path, mode='r') as file:
         text = file.read().replace("\n", " ")
     file.close()
     return text
 
+
 def store_data(word_list: list, out_path_pkl: str):
-    with gfile.Open(out_path_pkl, 'w') as file:
+    with gfile.GFile(name=out_path_pkl, mode='w') as file:
         pickle.dump(word_list, file)
 
+
 # Main -----------------------------------------------------------------------------------------------------------------
-def run_component(args):
+def run_component(text_path:str, out_path_pkl:str):
     logging.info('Initiating Data Preparation component...')
-    text_path = args.text_path
-    out_path_pkl = args.path_pkl
     try:
         text = load_text(file_path=text_path)
         token_text = run_prepare(text)
         store_data(word_list=token_text,
-               out_path_pkl=out_path_pkl)
+                   out_path_pkl=out_path_pkl)
     except RuntimeError as error:
         logging.info(error)
     else:
         logging.info('Data Preparation component successfully completed!')
     return out_path_pkl
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Run Data Preprocess")
+
+    parser.add_argument('--text-path',
+                        type=str,
+                        required=True,
+                        help='Path of text to process')
+
+    parser.add_argument('--pkl-path',
+                        type=str,
+                        required=True,
+                        help='Path to store pickle')
+
+    args = parser.parse_args()
+    text_path = args.text_path
+    out_path_pkl = args.pkl_path
+
+    run_component(text_path=text_path, out_path_pkl=out_path_pkl)
--- MachineLearningPlatforms/Kubeflow/base/pipelines/store_data_v2/components/prepare/src/prepare.py
+++ MachineLearningPlatforms/Kubeflow/base/pipelines/store_data_v2/components/prepare/src/prepare.py
@@ -4,13 +4,8 @@
 # prepare is a simple example of data preparation component
 
 import nltk
-import logging
 import argparse
 
-# Settings
-logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
-                    datefmt='%m/%d/%Y %I:%M:%S %p',
-                    level=logging.DEBUG)
 
 # Functions ------------------------------------------------------------------------------------------------------------
 def prepare_data(text: str) -> list:
@@ -18,16 +13,12 @@ def prepare_data(text: str) -> list:
     plain_text = tokenizer.tokenize(text=text)
     return plain_text
 
+
 # Main -----------------------------------------------------------------------------------------------------------------
-def run_prepare(args):
-    logging.info('Data preparation process starts...')
-    try:
-        token_text = prepare_data(text=text)
-    except RuntimeError as error:
-        logging.info(error)
-    else:
-        logging.info('Data preparation process successfully completed!')
+def run_prepare(text: str):
+    token_text = prepare_data(text=text)
     return token_text
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run Data Preprocess")

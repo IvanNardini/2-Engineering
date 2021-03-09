fc90ad6a53a107b827e458b709ac4d4901bce6e3
IvanNardini <ivan.nardini92@gmail.com>
2021-03-09 11:09:31 +0100

    typo

--- MachineLearningPlatforms/Kubeflow/ml-components/1_datapreparation/women_clothing_ecom_reviews/pipeline/components/data_collection/build.sh
+++ MachineLearningPlatforms/Kubeflow/ml-components/1_datapreparation/women_clothing_ecom_reviews/pipeline/components/data_collection/build.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
-docker build -t data-collect:1.0.0 .
-docker container run -it -p 9090:9090 --rm data-collect:1.0.0
+DOCKER_HOST=${1:-docker.io}
+DOCKER_REPO=${2:-in92}
+DOCKER_IMAGE=${3:-data_collect:1.0.0}
+MODE=${4:-""}
+
+docker build -t "$DOCKER_IMAGE" .
+docker tag "$DOCKER_IMAGE" "$DOCKER_HOST/$DOCKER_REPO/$DOCKER_IMAGE"
+docker push "$DOCKER_HOST/$DOCKER_IMAGE"
+
+if [ -z "$MODE" ]; then
+  docker container run -it -p 9090:9090 --rm $DOCKER_IMAGE
+fi

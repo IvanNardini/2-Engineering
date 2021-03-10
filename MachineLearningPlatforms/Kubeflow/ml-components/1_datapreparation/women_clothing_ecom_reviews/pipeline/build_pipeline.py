856c29612e6b4569f7533d4b9c2381a8da7cc66a
IvanNardini <ivan.nardini92@gmail.com>
2021-03-10 08:20:12 +0100

    refractor code

--- MachineLearningPlatforms/Kubeflow/ml-components/1_datapreparation/women_clothing_ecom_reviews/pipeline/build_pipeline.py
+++ MachineLearningPlatforms/Kubeflow/ml-components/1_datapreparation/women_clothing_ecom_reviews/pipeline/build_pipeline.py
@@ -9,13 +9,18 @@ import kfp.dsl as dsl
 from kfp.gcp import use_gcp_secret
 
 import datetime
+from uri import URI
 import os
 import argparse
 
 # Variables ------------------------------------------------------------------------------------------------------------
+GCS_COMPONENT_PATH = 'components/config_init/component.yaml'
 REGISTRY = "docker.io/in92"
 
 # Components -----------------------------------------------------------------------------------------------------------
+
+gcs_download_component = kfp.components.load_component_from_file(filename=GCS_COMPONENT_PATH)
+
 @kfp.dsl.component
 def data_collection(config_file: dsl.PipelineParam):
     return kfp.dsl.ContainerOp(
@@ -31,8 +36,16 @@ def run_build_pipeline(args):
 
     @dsl.pipeline(name="Women Clothing Reviews Classification ML Pipeline",
                   description="An example of Machine Learning Pipeline")
-    def build_pipeline(config_file: dsl.PipelineParam):
-        step_1 = data_collection(config_file=config_file)
+    def build_pipeline(config_file: URI):
+        # General setting
+        out_vol_op = dsl.VolumeOp(name='Persistent Volume',
+                                  resource_name='data',
+                                  size="3Gi",
+                                  modes=dsl.VOLUME_MODE_RWO)
+        step_0 = gcs_download_component(config_file)
+        step_0.add_pvolumes({'/pipe-config': out_vol_op.volume})
+        step_1 = data_collection(config_file=step_0.output)
+        step_1.after(step_0)
 
     pipeline_complier = cmp.Compiler()
     pipeline_complier.compile(pipeline_func=build_pipeline,

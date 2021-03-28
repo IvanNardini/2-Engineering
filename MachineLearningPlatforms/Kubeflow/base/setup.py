797cab8343e0a47554c67a23a69fd73f11541144
IvanNardini <ivan.nardini92@gmail.com>
2021-03-28 14:14:56 +0200

    remove base

--- MachineLearningPlatforms/Kubeflow/base/setup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-from setuptools import setup, find_packages
-
-setup(name='hellokubeflow',
-      extras_required=dict(tests=['pytest']),
-      packages=find_packages(where='pipelines'),
-      package_dir={"": "pipelines"})
